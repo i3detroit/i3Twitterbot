7da99a93eaab8defe9c4ac6d2f975457acc786b6
Andrew G. Meyer <agmlego@gmail.com>
2013-12-29 02:53:26 +0000

    Added logic for demo mode, showing off time, date, and countdown sequence.

--- code/PT6961.py
+++ code/PT6961.py
@@ -25,74 +25,82 @@ _DISPLAY_14_16 = 0x8F
 
 addresses = (0xC0,0xC2,0xC4,0xC6)
 
-font = {	0:  0xFC,
-		1:  0x60,
-		2:  0xDA,
-		3:  0xF2,
-		4:  0x66,
-		5:  0xB6,
-		6:  0xBE,
-		7:  0xE0,
-		8:  0xFE,
-		9:  0xF6,
-		'A':0xEE,
-		'B':0x3E,
-		'C':0x9C,
-		'D':0x7A,
-		'E':0x9E,
-		'F':0x8E
-	}
+font = {    0:  0xFC,
+        1:  0x60,
+        2:  0xDA,
+        3:  0xF2,
+        4:  0x66,
+        5:  0xB6,
+        6:  0xBE,
+        7:  0xE0,
+        8:  0xFE,
+        9:  0xF6,
+        'A':0xEE,
+        'B':0x3E,
+        'C':0x9C,
+        'D':0x7A,
+        'E':0x9E,
+        'F':0x8E
+    }
 
 def fixEndian(b):
-	fbyte = int('{:08b}'.format(b)[::-1],2)
-	#print 'Flipped: %02X -> %02X'%(b,fbyte)
-	return fbyte
+    fbyte = int('{:08b}'.format(b)[::-1],2)
+    #print 'Flipped: %02X -> %02X'%(b,fbyte)
+    return fbyte
 
 def initDisplay():
-	sendCmd(_DISPLAY_6X12)
-	sendCmd(_AUTO_INCREMENT)
-	initRAM()
-	sendCmd(_DISPLAY_14_16)
-	print 'Init Display'
+    sendCmd(_DISPLAY_6X12)
+    sendCmd(_AUTO_INCREMENT)
+    initRAM()
+    sendCmd(_DISPLAY_14_16)
+    print 'Init Display'
 
 def initRAM():
-	port.writebytes([fixEndian(0xC0)] + [0x00] * 8)
-	print 'Init RAM'
+    port.writebytes([fixEndian(0xC0)] + [0x00] * 8)
+    print 'Init RAM'
 
 def sendCmd(cmd):
-	port.writebytes([fixEndian(cmd)])
-	print 'Sending CMD %s'%cmd
+    port.writebytes([fixEndian(cmd)])
+    print 'Sending CMD %s'%cmd
 
 def sendCmds(*cmds):
-	port.writebytes(map(fixEndian,cmds))
-	print 'Sending CMDs %s'%str(cmds)
+    port.writebytes(map(fixEndian,cmds))
+    print 'Sending CMDs %s'%str(cmds)
 
 def sendDigit(digit,val):
-	port.writebytes([fixEndian(addresses[digit]),font[val]])
-	print 'Sending digit %s value %s'%(digit,val)
+    port.writebytes([fixEndian(addresses[digit]),font[val]])
+    print 'Sending digit %s value %s'%(digit,val)
 
-def sendNum(num,colon=False):
-	digits = map(int,'%04d'%num)
-	print 'Sending %s as %s (%s)'%(num,digits,('no colon','with colon')[colon])
-	sendDigits(digits,colon)
+def sendNum(num,low_dot=False,high_dot=False):
+    digits = map(int,'%04d'%num)
+    print 'Sending %s as %s (%s %s)'%(num,digits,('no low','low')[low_dot],('no high','high')[high_dot])
+    sendDigits(digits,low_dot,high_dot)
 
-def sendDigits(digits,colon=False):
-	digits = [font[d] for d in digits]
-	if colon:
-		digits[0] |= 0x1
-		digits[1] |= 0x1
-	d = []
-	for addr,digit in zip(addresses,digits):
-		d.append(fixEndian(addr))
-		d.append(digit)
-	print 'Sending %s as %s'%(digits,d)
-	port.writebytes(d)
+def sendDigits(digits,low_dot=False,high_dot=False):
+    digits = [font[d] for d in digits]
+    if low_dot:
+        digits[1] |= 0x1
+    if high_dot:
+        digits[0] |= 0x1
+    d = []
+    for addr,digit in zip(addresses,digits):
+        d.append(fixEndian(addr))
+        d.append(digit)
+    print 'Sending %s as %s'%(digits,d)
+    port.writebytes(d)
 
 if __name__ == "__main__":
-	port.msh = 12000000
-	initDisplay()
-	while True:
-		t = datetime.now()
-		print t
-		sendNum(t.hour*100+t.minute,t.second%2)
-		sleep(1)
+    port.msh = 12000000
+    initDisplay()
+    while True:
+        t = datetime.now()
+        print t
+        if t.second/30:
+            if t.second%10 in xrange(10/2):
+                sendNum(t.hour*100+t.minute,low_dot=True,high_dot=True)
+            else:
+                sendNum(t.month*100+t.day,low_dot=True,high_dot=False)
+            sleep(1)
+        else:
+            sendNum((29-t.second)*100+99-(t.microsecond/10000),high_dot=True)
+            sleep(0.01)
